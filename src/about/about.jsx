badaa8c1e84e30d8d714aed6c17c5dd6197a411c
David Sylvestre <david.sylvestre@hotmail.com>
2017-05-31 13:59:33 -0300

    Inicio do formulario e lista

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import PageHeader from '../template/pageHeader'
 
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+    <h2>Nossa história</h2>
+    <p>Blablabla</p>
+    <p>Blablabla</p>
+    <p>Blablabla</p>
+    <h2>Imprensa</h2>
+    <p>Blablabla</p>
+    <p>Blablabla</p>
+    <p>Blablabla</p>
   </div>
 )
