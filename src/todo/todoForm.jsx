46f78438c8f39ab4f64f78ea48a32d3dbb7bf62f
David Sylvestre <david.sylvestre@hotmail.com>
2017-06-01 14:37:00 -0300

    inclusão do component Grid

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Grid from '../template/grid'
 
 
 export default props => (
   <div>
-    <h1>Form</h1>
+    <div role="form" className="todoForm">
+      <Grid cols='12 9 10'>
+        <input id="description" type="text" className="form-control"/>
+      </Grid>
+
+      <Grid cols="12 3 2">
+        <button className="btn btn-primary">
+          <i className="fa fa-plus"></i>
+        </button>
+      </Grid>
+    </div>
   </div>
 )
